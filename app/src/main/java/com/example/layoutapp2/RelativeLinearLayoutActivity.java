946ee72826582b0a2280278c3e18a0c7a4b314e0
Junior28 <ejniormartinez@uniguajira.edu.co>
2020-10-18 09:14:12 -0500

    Agregar RelativeLinearLayout

---
+++ app/src/main/java/com/example/layoutapp2/RelativeLinearLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_linear_layout);
+    }
+}
