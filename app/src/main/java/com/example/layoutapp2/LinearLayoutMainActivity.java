fb76debcc67ddf46de78a682edae6ce845a0d3a3
Junior28 <ejniormartinez@uniguajira.edu.co>
2020-10-17 21:34:00 -0500

    Agregando una nueva Activity

---
+++ app/src/main/java/com/example/layoutapp2/LinearLayoutMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLayoutMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
